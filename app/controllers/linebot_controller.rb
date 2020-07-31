a60f8536110313350c31d2ebd57e02114162ca95
zusaaaa <obbyokamoto@gmail.com>
2020-07-31 11:41:14 +0900

    CSRF_impliment
    CSRF対策をapplication_controllerに記述
    linebot_controllerのインデント調整

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,23 +1,7 @@
 class LinebotController < ApplicationController
-  require 'line/bot'
-
-  protect_from_forgery :except :callback
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
 
   def callback
     body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head :bad_request
-    end
-
     events = client.parse_events_from(body)
 
     events.each { |event|
@@ -39,27 +23,26 @@ class LinebotController < ApplicationController
 
   private
 
-  def template
-    {
+    def template {
       "type": "template",
       "altText": "this is a confirm template",
       "template": {
-          "type": "confirm",
-          "text": "!a とは aがtrueの場合に false を、aがfalseの場合にtrueを返す論理演算子である"
-          "actions": [
-              {
-                "type": "message",
-                # Botから送られてきたメッセージに表示される文字列です。
-                "label": "○",
-                # ボタンを押した時にBotに送られる文字列です。
-                "text": "○"
-              },
-              {
-                "type": "message",
-                "label": "×",
-                "text": "×"
-              }
-          ]
+        "type": "confirm",
+        "text": "!a とは aがtrueの場合に false を、aがfalseの場合にtrueを返す論理演算子である"
+        "actions": [
+          {
+          "type": "message",
+            # Botから送られてきたメッセージに表示される文字列です。
+            "label": "○",
+            # ボタンを押した時にBotに送られる文字列です。
+            "text": "○"
+          },
+          {
+          "type": "message",
+          "label": "×",
+          "text": "×"
+          }
+        ]
       }
     }
   end
